8e541cbd2051d4e9cb0b7c06de7767cf730e0a04
Leandro Arturi <lea.arturi@gmail.com>
2025-02-03 21:11:56 -0300

    Protejo variables de entorno

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_API_KEY: string
+  readonly PUBLIC_AUTH_DOMAIN: string
+  readonly PUBLIC_PROJECT_ID: string
+  readonly PUBLIC_STORAGE_BUCKET: string
+  readonly PUBLIC_MESSAGING_SENDER_ID: string
+  readonly PUBLIC_APP_ID: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,12 +6,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAtdK8SNH12bmigHD0-zpyHDuHvTpsgcn4',
-  authDomain: 'astro-auth-udemy.firebaseapp.com',
-  projectId: 'astro-auth-udemy',
-  storageBucket: 'astro-auth-udemy.firebasestorage.app',
-  messagingSenderId: '827464083413',
-  appId: '1:827464083413:web:566662fa17d4afd8e4bf5c'
+  apiKey: import.meta.env.PUBLIC_API_KEY,
+  authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
+  projectId: import.meta.env.PUBLIC_PROJECT_ID,
+  storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.PUBLIC_MESSAGING_SENDER_ID,
+  appId: import.meta.env.PUBLIC_APP_ID
 }
 
 // Initialize Firebase
