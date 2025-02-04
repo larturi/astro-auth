90db128769f88c8b92e13b5eb21ad4eec301332a
Leandro Arturi <lea.arturi@gmail.com>
2025-02-04 19:19:47 -0300

    Add public route handling and redirect logic in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,21 @@ import type { MiddlewareNext } from 'astro'
 import { firebase } from './firebase/config'
 
 const privateRoutes = ['/protected']
+const publicRoutes = ['/login', '/register']
 
-export const onRequest = async (context: any, next: any) => {
-  if (privateRoutes.includes(context.url.pathname)) {
-    const authHeader = context.request.headers.get('authorization')
+export const onRequest = async (
+  { redirect, request, url, locals }: any,
+  next: any
+) => {
+  if (privateRoutes.includes(url.pathname)) {
+    const authHeader = request.headers.get('authorization')
   }
 
   const isLoggedIn = !!firebase.auth.currentUser
   const user = firebase.auth.currentUser
 
   if (user) {
-    context.locals.user = {
+    locals.user = {
       email: user.email,
       name: user.displayName,
       avatar: user.photoURL,
@@ -20,7 +24,17 @@ export const onRequest = async (context: any, next: any) => {
     }
   }
 
-  context.locals.isLoggedIn = isLoggedIn
+  locals.isLoggedIn = isLoggedIn
+
+  // El usuario no esta logueado y esta intentando acceder a una ruta privada
+  if (!isLoggedIn && privateRoutes.includes(url.pathname)) {
+    return redirect('/login')
+  }
+
+  // El usuario esta logueado y esta intentando acceder a una ruta publica
+  if (isLoggedIn && publicRoutes.includes(url.pathname)) {
+    return redirect('/')
+  }
 
   return next()
 }
